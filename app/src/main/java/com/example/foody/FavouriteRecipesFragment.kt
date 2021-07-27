50d73e0d81b8bca136279ace306d208da1f9593d
AkinchanKushwaha <akinchandroger@gmail.com>
2021-07-27 15:12:16 +0530

    Navigation component setup

---
+++ app/src/main/java/com/example/foody/FavouriteRecipesFragment.kt
@@ -0,0 +1,17 @@
+package com.example.foody
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FavouriteRecipesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favourite_recipes, container, false)
+    }
+}
