858e914c75ad4764e77b1f8bae86970c0a67e66c
AkinchanKushwaha <akinchandroger@gmail.com>
2021-09-02 14:16:15 +0530

    added ic_error_placeholder

--- app/src/main/java/com/example/foody/bindingAdapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foody/bindingAdapters/RecipesRowBinding.kt
@@ -15,6 +15,7 @@ class RecipesRowBinding {
         fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
             imageView.load(imageUrl) {
                 crossfade(600)
+                error(R.drawable.ic_error_placeholder)
             }
         }
 
