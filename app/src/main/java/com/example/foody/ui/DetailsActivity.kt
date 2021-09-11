8a847c7190a0e1736c8d73316378b21f39ee8275
AkinchanKushwaha <akinchandroger@gmail.com>
2021-09-11 10:51:45 +0530

    Designed DetailsActivity

---
+++ app/src/main/java/com/example/foody/ui/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.foody.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.foody.R
+
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
