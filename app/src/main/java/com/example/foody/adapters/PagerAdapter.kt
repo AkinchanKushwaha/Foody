378576f80fe8d033788eb5b4dd8936bb28e86f8f
AkinchanKushwaha <akinchandroger@gmail.com>
2021-09-11 11:41:50 +0530

    created viewPager class

---
+++ app/src/main/java/com/example/foody/adapters/PagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.foody.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(
+    private val resultBundle: Bundle,
+    private val fragments: ArrayList<Fragment>,
+    private val title: ArrayList<String>,
+    private val fm: FragmentManager
+) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        fragments[position].arguments = resultBundle
+        return fragments[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return title[position]
+    }
+}
---
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.foody.ui.fragments.instructions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foody.R
+
+class InstructionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    }
+}
