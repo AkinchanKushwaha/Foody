d70dfe9352850967057f9812ee2c68089c0b9c63
AkinchanKushwaha <akinchandroger@gmail.com>
2021-09-23 23:28:49 +0530

    designed fragment_food_joke

--- app/src/main/java/com/example/foody/ui/fragments/favourites/FavouriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/favourites/FavouriteRecipesFragment.kt
@@ -31,7 +31,7 @@ class FavouriteRecipesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentFavouriteRecipesBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
