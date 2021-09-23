77b6dd43b0942c9bdbb5a246df908e390e2f36ac
AkinchanKushwaha <akinchandroger@gmail.com>
2021-09-23 23:35:34 +0530

    created new get request for FoodJoke

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -1,8 +1,10 @@
 package com.example.foody.data.network
 
+import com.example.foody.models.FoodJoke
 import com.example.foody.models.FoodRecipe
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface FoodRecipesApi {
@@ -15,4 +17,9 @@ interface FoodRecipesApi {
     suspend fun searchRecipes(
         @QueryMap searchQuery: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("/food/jokes/random")
+    suspend fun getFoodJoke(
+        @Query("apiKey") apiKey: String
+    ): Response<FoodJoke>
 }
